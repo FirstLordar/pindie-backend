ef8147b0886558385eaf7395499a73a15d424419
FirstLordar <arzhan2517@gmail.com>
2024-05-27 19:03:34 +0300

    8

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,4 @@
 const allowedCors = [
-    'https://practicum.yandex.ru',
-    'https://students-projects.ru',
-    'http://localhost:3000',
-    'http://localhost:3001',
     'https://market.pindie.nomoredomainswork.ru'
 ];
 
@@ -10,9 +6,9 @@ function cors(req, res, next) {
     const { origin } = req.headers;
     if (allowedCors.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     }
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
 }
 
