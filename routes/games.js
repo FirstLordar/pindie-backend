2bb5cee038b8a3ae3c56f4ff85ad154bb9c1b0a6
FirstLordar <arzhan2517@gmail.com>
2024-05-27 18:05:12 +0300

    попытка 4

--- routes/games.js
+++ routes/games.js
@@ -1,14 +1,14 @@
 
 const gamesRouter = require('express').Router();
 
-const {findAllGames, createGame, findGameById, updateGame, deleteGame, checkEmptyFields, checkIfUsersAreSafe, checkIfCategoriesAvalible, checkIsGameExists, checkIsVoteRequest} = require('../middlewares/games');
-const {sendAllGames, sendCreatedGame, sendGameById, sendGameUpdated, sendGameDeleted} = require('../controllers/games');
-const {checkAuth} = require('../middlewares/auth');
+const { findAllGames, createGame, findGameById, updateGame, deleteGame, checkEmptyFields, checkIfUsersAreSafe, checkIsGameExists, checkIsVoteRequest, checkIfCategoriesAvalible } = require('../middlewares/games');
+const { sendAllGames, sendCreatedGame, sendGameById, sendGameUpdated, sendGameDeleted } = require('../controllers/games');
+const { checkAuth } = require('../middlewares/auth.js');
 
 gamesRouter.get('/games', findAllGames, sendAllGames);
-gamesRouter.post('/games', findAllGames,checkIsGameExists, checkIfCategoriesAvalible, checkEmptyFields, checkAuth , createGame, sendCreatedGame  )
-gamesRouter.get('/games/:id', findGameById, sendGameById)
-gamesRouter.put('/games/:id',findGameById,checkIsVoteRequest, checkIfUsersAreSafe, checkIfCategoriesAvalible, checkEmptyFields, checkAuth ,updateGame, sendGameUpdated)
-gamesRouter.delete('/games/:id', checkAuth, deleteGame, sendGameDeleted)
+gamesRouter.get('/games/:id', findGameById, sendGameById);
+gamesRouter.post('/games', findAllGames, checkIsGameExists, checkIfCategoriesAvalible, checkEmptyFields, checkAuth, createGame, sendCreatedGame);
+gamesRouter.put('/games/:id', findGameById, checkIsVoteRequest, checkIfUsersAreSafe, checkIfCategoriesAvalible, checkEmptyFields, checkAuth, updateGame, sendGameUpdated);
+gamesRouter.delete("/games/:id", checkAuth, deleteGame, sendGameDeleted);
 
 module.exports = gamesRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,24 @@
 const usersRouter = require('express').Router();
 
-const {findAllUsers, createUser, findUserById, updateUser, deleteUser, checkEmptyNameAndEmailAndPassword, checkEmptyNameAndEmail, checkIsUserExists, hashPassword, filterPassword} = require('../middlewares/users');
-const {sendAllUsers, sendCreatedUser, sendUserById, sendUserUpdated, sendUserDeleted, sendMe} = require('../controllers/users');
-const {checkAuth} = require('../middlewares/auth');
-
-usersRouter.get('/users', findAllUsers, filterPassword, sendAllUsers);
-usersRouter.post(
-    "/users",
-    findAllUsers,
+const { checkAuth } = require('../middlewares/auth.js');
+const { sendAllUsers, sendUserById, sendUserUpdated, sendUserDeleted, sendMe, sendCreatedUser } = require('../controllers/users');
+const { findAllUsers,
+    createUser,
+    findUserById,
+    updateUser,
+    deleteUser,
     checkIsUserExists,
     checkEmptyNameAndEmailAndPassword,
-    checkAuth,
-    hashPassword,
-    createUser,
-    sendCreatedUser
-  ); 
-usersRouter.get('/users:id', findUserById, filterPassword, sendUserById)
-usersRouter.put(
-    "/users/:id",
     checkEmptyNameAndEmail,
-    checkAuth,
-    updateUser,
-    sendUserUpdated
-  );
-  usersRouter.delete(
-    "/users/:id",
-    checkAuth,
-    deleteUser,
-    sendUserDeleted
-); 
+    filterPassword,
+    hashPassword } = require('../middlewares/users');
+
+
+usersRouter.get('/users', findAllUsers, filterPassword, sendAllUsers);
+usersRouter.get('/users/:id', findUserById, filterPassword, sendUserById);
 usersRouter.get("/me", checkAuth, sendMe);
+usersRouter.post('/users', findAllUsers, checkIsUserExists, checkEmptyNameAndEmailAndPassword, checkAuth, hashPassword, createUser, sendCreatedUser);
+usersRouter.put('/users/:id', checkEmptyNameAndEmail, checkAuth, updateUser, sendUserUpdated);
+usersRouter.delete("/users/:id", checkAuth, deleteUser, sendUserDeleted);
 
 module.exports = usersRouter;
